2c3a661e44a7b5bc7959b4fe08e8fae8a2a1e5a1
adnan ahmed <adnanahmed504423@gmail.com>
2023-06-20 18:32:39 +0530

    chap2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,38 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Awesome App")),
-      body: Container(
-        child: Text("hi flutter"),
-      ),
-    );
+        appBar: AppBar(title: Text("Awesome App")),
+        body: Container(
+          height: 100,
+          color: Colors.teal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.red,
+                alignment: Alignment.center,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+                alignment: Alignment.center,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                alignment: Alignment.center,
+              ),
+            ],
+          ),
+        ));
   }
 }
